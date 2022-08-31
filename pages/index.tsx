d89c267e90f4e609d59498c709c7f91ad586e157
Dalvin Molina <dalvingamer@gmail.com>
2022-08-30 21:07:31 -0400

    feat: console

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from 'next'
 
+
+
 const Home: NextPage = () => {
+
+  console.log(process.env, ' hola desde aqui... ');
+
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
