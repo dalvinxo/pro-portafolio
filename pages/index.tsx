8321ba0d1a0981412bc1fff9ee72e988238e59ae
Dalvin Molina <dalvingamer@gmail.com>
2022-08-30 23:06:51 -0400

    fix: delete file .env, no show repo

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
 
-  console.log(JSON.stringify(process.env, null, 2), process.env.NODE_ENV, process.env.CI, process.env.NOTION_SECRECT, ' hola desde aqui... ');
+  console.log(JSON.stringify(process.env, null, 2), process.env.NEXT_PUBLIC_NOTION_SECRECT, process.env.NODE_ENV, process.env.CI, process.env.NOTION_SECRECT, ' hola desde aqui... ');
 
   return (
     <h1 className="text-3xl font-bold underline">
