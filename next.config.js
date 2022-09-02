970f5c321b8c76d56dbf7b89683d7c9460c1e5a6
Dalvin Molina <dalvingamer@gmail.com>
2022-09-02 12:39:56 -0400

    configuration del .yml

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   swcMinify: true,
   env: {
     CI: "Deploy",
-    NEXT_PUBLIC_NOTION_SECRECT: process.env.NEXT_PUBLIC_NOTION_SECRECT,
+    NEXT_PUBLIC_NOTION_SECRECT: "Desde el modo desarrollo",
     NOTION_SECRECT: process.env.NOTION_SECRECT,
     NOTION_SECRECT_: "Intentando acceder"
   }
