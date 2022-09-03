1c7bc802a043902a270af64caa3d7f105b4e0abf
Dalvin Molina <dalvingamer@gmail.com>
2022-09-03 12:04:18 -0400

    refact: testeando key api notion

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    NEXT_PUBLIC_NOTION_SECRECT: process.env.NOTION_SECRECT,
-    NOTION_SECRECT: process.env.NOTION_SECRECT,
-    NOTION_SECRECT_: "Intentando acceder"
-  }
+  // env: {
+  //   NEXT_PUBLIC_NOTION_SECRECT: process.env.NOTION_SECRECT,
+  //   NOTION_SECRECT: process.env.NOTION_SECRECT,
+  //   NOTION_SECRECT_: "Intentando acceder"
+  // }
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,22 @@
 import { Client } from '@notionhq/client'
 import { ListBlockChildrenResponse } from '@notionhq/client/build/src/api-endpoints'
 import type { GetStaticProps, NextPage } from 'next'
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
 
 export const getStaticProps: GetStaticProps = async (context) => {
 
   const notion = new Client({
     auth: process.env.NOTION_SECRECT,
+  });
+
+  const data = await notion.blocks.children.list({
+    block_id: process.env.PAGE_ID || '',
   })
-
-  console.log(notion);
-
-  // const data = await notion.blocks.children.list({
-  //     block_id: process.env.PAGE_ID || "dwmaoid2",
-  // })
-
 
   return {
     props: {
-      info: {}
+      info: data
     }
   }
 
@@ -32,29 +29,13 @@ interface Props {
 
 const Home: NextPage<Props> = ({ info }) => {
 
-  console.log('NEXT_PUBLIC_NOTION_SECRECT:' + process.env.NEXT_PUBLIC_NOTION_SECRECT, 'NOTION_SECRECT' + process.env.NOTION_SECRECT, 'NOTION_SECRECT_ ' + process.env.NOTION_SECRECT_, 'NEXT_PUBLIC_NOTION_SECRECT_TP' + process.env.NEXT_PUBLIC_NOTION_SECRECT_TP)
-
-
-
-
-  // const [data, setData] = useState({})
-
-  // useEffect(() => {
-  //   fetch('/api/notion')
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setData(data)
-  //     })
-  // }, [])
-
-
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
       <pre>
-        {/* {JSON.stringify(data || {}, null, 2)} */}
+        {JSON.stringify(info || {}, null, 2)}
       </pre>
 
     </div>
