328ab797be4aa43622b6bea93ecf355ec1874c96
Dalvin Molina <dalvingamer@gmail.com>
2022-09-03 12:45:40 -0400

    feat: property experiimental image config.next.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 
+const { allowedNodeEnvironmentFlags } = require('process')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    images: {
+      allowFutureImage: false
+    },
+    external: true,
+    allowedNodeEnvironmentFlags: true
+  },
   env: {
     PAGE_ID: process.env.PAGE_ID,
     NOTION_SECRECT: process.env.NOTION_SECRECT
