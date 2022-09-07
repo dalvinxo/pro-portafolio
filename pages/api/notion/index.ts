cdfae180674ba95646e0e1ecc026384b4ed176ac
Dalvin Molina <dalvingamer@gmail.com>
2022-09-06 21:01:00 -0400

    feat

--- pages/api/notion/index.ts
+++ pages/api/notion/index.ts
@@ -1,11 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import { Client } from '@notionhq/client'
-// import { ListBlockChildrenResponse } from '@notionhq/client/build/src/api-endpoints'
+import { Client } from '@notionhq/client'
+import { ListBlockChildrenResponse } from '@notionhq/client/build/src/api-endpoints'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
     name: string,
-    // data: ListBlockChildrenResponse
+    data: ListBlockChildrenResponse
 }
 
 export default async function handler(
@@ -13,13 +13,14 @@ export default async function handler(
     res: NextApiResponse<Data>
 ) {
 
-    // const notion = new Client({
-    //     auth: process.env.NOTION_SECRECT,
-    // })
+    const notion = new Client({
+        auth: process.env.NOTION_SECRECT
+      });
+    
+      const data = await notion.blocks.children.list({
+        block_id: process.env.PAGE_ID || 'af18787b0125493ab83aa2e240a01e29'
+      })
+    
 
-    // const data = await notion.blocks.children.list({
-    //     block_id: process.env.PAGE_ID || "dwmaoid2",
-    // })
-
-    res.status(200).json({ name: 'John Doe' })
+    res.status(200).json({ data, name: 'John Doe' })
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,38 @@
-import { Client } from '@notionhq/client'
-import { ListBlockChildrenResponse } from '@notionhq/client/build/src/api-endpoints';
-import type { GetServerSideProps, NextPage } from 'next'
+// import { Client } from '@notionhq/client'
+// import { ListBlockChildrenResponse } from '@notionhq/client/build/src/api-endpoints';
+import type { NextPage } from 'next'
+import useSWR from 'swr';
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
+// export const getServerSideProps: GetServerSideProps = async () => {
 
   
-  const notion = new Client({
-    auth: process.env.NOTION_SECRECT
-  });
+  // const notion = new Client({
+  //   auth: process.env.NOTION_SECRECT
+  // });
 
-  const data = await notion.blocks.children.list({
-    block_id: process.env.PAGE_ID || 'af18787b0125493ab83aa2e240a01e29'
-  })
+  // const data = await notion.blocks.children.list({
+  //   block_id: process.env.PAGE_ID || 'af18787b0125493ab83aa2e240a01e29'
+  // })
 
-  return {
-    props: {
-      info: data
-    }
-  }
+  // return {
+  //   props: {
+  //     info: data
+  //   }
+  // }
 
-}
+// }
 
-interface Props {
-  info?: ListBlockChildrenResponse;
-}
+// interface Props {
+//   info?: ListBlockChildrenResponse;
+// }
+const fetcher = (url: RequestInfo | URL) => fetch(url).then((res) => res.json())
 
+const Home: NextPage = () => {
 
-const Home: NextPage<Props> = ({ info }) => {
+  const { data } = useSWR('/api/notion', fetcher)
+
+  const info = data;
 
   return (
     <div>
