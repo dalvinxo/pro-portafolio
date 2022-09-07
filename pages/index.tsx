1ba773f79f19bd871b8901a45143455c023141fe
Dalvin Molina <dalvingamer@gmail.com>
2022-09-06 22:28:42 -0400

    ---

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,36 @@
-// import { Client } from '@notionhq/client'
-// import { ListBlockChildrenResponse } from '@notionhq/client/build/src/api-endpoints';
-import type { NextPage } from 'next'
-import useSWR from 'swr';
+import { Client } from '@notionhq/client'
+import { ListBlockChildrenResponse } from '@notionhq/client/build/src/api-endpoints';
+import type { GetStaticProps, NextPage } from 'next'
 
-
-// export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   
-  // const notion = new Client({
-  //   auth: process.env.NOTION_SECRECT
-  // });
+  const notion = new Client({
+    auth: process.env.NOTION_SECRECT
+  });
 
-  // const data = await notion.blocks.children.list({
-  //   block_id: process.env.PAGE_ID || 'af18787b0125493ab83aa2e240a01e29'
-  // })
+  const data = await notion.blocks.children.list({
+    block_id: process.env.PAGE_ID || 'af18787b0125493ab83aa2e240a01e29'
+  })
 
-  // return {
-  //   props: {
-  //     info: data
-  //   }
-  // }
+  return {
+    props: {
+      info: data
+    }
+  }
 
-// }
+}
 
-// interface Props {
-//   info?: ListBlockChildrenResponse;
-// }
-const fetcher = (url: RequestInfo | URL) => fetch(url).then((res) => res.json())
+interface Props {
+  info?: ListBlockChildrenResponse;
+}
+// const fetcher = (url: RequestInfo | URL) => fetch(url).then((res) => res.json())
 
-const Home: NextPage = () => {
+const Home: NextPage<Props> = ({info}) => {
 
-  const { data } = useSWR('/api/notion', fetcher)
+  // const { data } = useSWR('/api/notion', fetcher)
 
-  const info = data;
+  // const info = data;
 
   return (
     <div>
