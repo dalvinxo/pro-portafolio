1f4a86759f24bd6956a852b20822351429167b1a
Dalvin Molina <dalvingamer@gmail.com>
2022-10-05 19:02:17 -0400

    feat: structure component Portafolio

--- src/components/Portafolio/index.tsx
+++ src/components/Portafolio/index.tsx
@@ -1,7 +1,32 @@
+import Image from 'next/image'
 const Portafolio = () => {
   return (
     <>
-      <div></div>
+      <div>
+        <hr />
+        <div>
+          <div>
+            <div>
+              <Image
+                src={'./img/avatar_profile.png'}
+                alt={'img present portafoly'}
+                height={400}
+                width={400}
+              />
+            </div>
+            <div className="container">
+              <div className=""></div>
+              <div className="py-3 px-3">
+                <div className="container grid grid-cols-3 space-x-2">
+                  <div>Github</div>
+                  <div>View</div>
+                  {/* <div></div> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
