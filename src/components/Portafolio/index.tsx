01b17dbbf7c9b15667a5089cef18fdbed4e2b590
Dalvin David Molina Ogando <dalvin.molina@pgr.gob.do>
2022-10-13 14:28:52 -0400

    fix: img url component Portafolio

--- src/components/Portafolio/index.tsx
+++ src/components/Portafolio/index.tsx
@@ -1,33 +1,14 @@
-import Image from 'next/image'
-const Portafolio = () => {
+import { NextPage } from 'next'
+//import Image from 'next/image'
+
+const Portafolio: NextPage = () => {
   return (
-    <>
+    <div>
+      <hr />
       <div>
-        <hr />
-        <div>
-          <div>
-            <div>
-              <Image
-                src={'./img/avatar_profile.png'}
-                alt={'img present portafoly'}
-                height={400}
-                width={400}
-              />
-            </div>
-            <div className="container">
-              <div className=""></div>
-              <div className="py-3 px-3">
-                <div className="container grid grid-cols-3 space-x-2">
-                  <div>Github</div>
-                  <div>View</div>
-                  {/* <div></div> */}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <div></div>
       </div>
-    </>
+    </div>
   )
 }
 
