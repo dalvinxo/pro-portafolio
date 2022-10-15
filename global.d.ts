ee054531f1b8df207d73063c291b9b3e80d9a83c
Dalvin Molina <dalvingamer@gmail.com>
2022-10-15 19:27:43 -0400

    style: space with between paramater Navbar component

--- global.d.ts
+++ global.d.ts
@@ -1,11 +1,5 @@
-import { ReactNode } from 'react'
+// import { ReactNode } from 'react'
 
 export {}
 
-declare global {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  interface ElementWithChildren<T = {}> {
-    children: ReactNode
-    props?: T
-  }
-}
+declare global {}
