107b377e6f9c2da3baf58e2340ac426838e756ab
Dalvin Molina <dalvingamer@gmail.com>
2022-10-16 01:59:13 -0400

    fix: deploy error

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,11 @@
-// import { ReactNode } from 'react'
+import { ReactNode } from 'react'
 
 export {}
 
-declare global {}
+declare global {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  interface ElementWithChildren<T = {}> {
+    children?: ReactNode
+    props?: T
+  }
+}
