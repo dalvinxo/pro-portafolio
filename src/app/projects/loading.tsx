e748e574f006c1fc6e7e8bf1b56dbbf965fc1659
Dalvin Molina <dalvingamer@gmail.com>
2023-11-12 18:19:48 -0400

    style: add new style to the loading

--- src/app/projects/loading.tsx
+++
@@ -1,5 +0,0 @@
-import LoadPage from '@components/Loading'
-
-export default function Loading() {
-  return <LoadPage />
-}
--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,10 +1,21 @@
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="w-full flex flex-col justify-center items-center h-[60vh]">
       <div
-        className="animate-pulse flex justify-center items-center p-1 bg-slate-600 text-white rounded-md opacity-0"
+        className="animate-pulse w-full rounded-lg shadow-md p-4 m-5 flex flex-col justify-center bg-slate-500/80 text-white"
         role="status">
-        <span> Loading...</span>
+        <div className="w-2/3 h-4 bg-gray-300 rounded mb-2"></div>
+        <div className="w-full h-8 bg-gray-300 rounded mb-2"></div>
+        <div className="w-full h-8 bg-gray-300 rounded mb-2"></div>
+        <div className="w-1/2 h-8 bg-gray-300 rounded"></div>
+      </div>
+      <div
+        className="animate-pulse w-full rounded-lg shadow-md p-4 m-5 flex flex-col justify-center bg-slate-500/80 text-white"
+        role="status">
+        <div className="w-2/3 h-4 bg-gray-300 rounded mb-2"></div>
+        <div className="w-full h-8 bg-gray-300 rounded mb-2"></div>
+        <div className="w-full h-8 bg-gray-300 rounded mb-2"></div>
+        <div className="w-1/2 h-8 bg-gray-300 rounded"></div>
       </div>
     </div>
   )
