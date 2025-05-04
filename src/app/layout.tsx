31f089499fc5f4345aed94239c0fa88f22d148f8
dalvinxo <dalvingamer@gmail.com>
2025-05-03 22:39:26 -0400

    fix: remove trailing commas in metadata titles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,16 @@ import { Metadata, Viewport } from 'next'
 export const metadata: Metadata = {
   description:
     "Dalvinxo is a portafoly for review skill and project to the 'junior developer'",
+
   keywords: 'HTML, Link, next, react',
   title: 'Home',
+
   authors: {
     name: 'dalvinxo',
   },
+
   manifest: '/manifest.json',
+
   icons: {
     icon: '/favicon.ico',
     apple: ['/apple-touch-icon.png?v=4'],
