e116dca7a022bb6b1ce05b4f627abfb9351f939a
dalvinxo <dalvingamer@gmail.com>
2025-05-03 22:42:28 -0400

    feat: actualizando los params a la nueva version

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,3 +1,4 @@
-export default function Page({ params }: { params: { id: string } }) {
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params
   return <div>My Post: {params.id}</div>
 }
