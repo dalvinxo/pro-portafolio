c1ecaff0260ed43321a92d914cff69300bb0cb12
dalvinxo <dalvingamer@gmail.com>
2026-05-18 09:58:21 -0400

    style: improve code formatting and readability in detectBrowserLanguage function

--- src/utils/detectBrowserLanguage.ts
+++ src/utils/detectBrowserLanguage.ts
@@ -5,23 +5,23 @@ import { translate } from 'var-contants'
  * @returns El idioma detectado ('en' o 'es') o 'en' por defecto
  */
 export const detectBrowserLanguage = (): translate.Locales => {
-    if (typeof window === 'undefined') {
-        return 'en'
-    }
-
-    const supportedLocales: translate.Locales[] = ['en', 'es']
-
-    const browserLanguages = navigator.languages || [navigator.language]
-
-    for (const browserLang of browserLanguages) {
-
-        const langCode = browserLang.split('-')[0].toLowerCase() as translate.Locales
-
-
-        if (supportedLocales.includes(langCode)) {
-            return langCode
-        }
-    }
-
+  if (typeof window === 'undefined') {
     return 'en'
+  }
+
+  const supportedLocales: translate.Locales[] = ['en', 'es']
+
+  const browserLanguages = navigator.languages || [navigator.language]
+
+  for (const browserLang of browserLanguages) {
+    const langCode = browserLang
+      .split('-')[0]
+      .toLowerCase() as translate.Locales
+
+    if (supportedLocales.includes(langCode)) {
+      return langCode
+    }
+  }
+
+  return 'en'
 }
